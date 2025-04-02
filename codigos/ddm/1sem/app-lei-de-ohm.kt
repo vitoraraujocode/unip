804f7493a2497f2f125b17bcaef95334d88c77ab
vitoraraujocode <vitor3araujo@gmail.com>
2025-04-02 16:50:19 -0300

    Resumi e coloquei comentários na parte de cima

--- codigos/ddm/1sem/app-lei-de-ohm.kt
+++ codigos/ddm/1sem/app-lei-de-ohm.kt
@@ -2,25 +2,30 @@
 // Função que calcula a tensão elétrica aplicada através da fórmula V = I * R.
 fun calcV() {
     print("Digite a corrente (I) em Amperes: ")
-    val I = readLine()?.toDoubleOrNull() ?: return println("Entrada inválida!") // Faz com que se a variavel intensidade elétrica for decimal ou nula irá retornar um println "Entrada inválida!", para evitar que a máquina dê erro.
+// Recebe a entrada do usuario e retorna erro caso seja inválida.
+    val I = readLine()?.toDoubleOrNull() ?: return println("Entrada inválida!") 
     val V = I * R
     print("Digite a resistência (R) em Ohms: ")
-    val R = readLine()?.toDoubleOrNull() ?: return println("Entrada inválida!") // Faz com que se a variavel resistência for decimal ou nula irá retornar um println "Entrada inválida!", para evitar que a máquina dê erro.
+// Recebe a entrada do usuario e retorna erro caso seja inválida.
+    val R = readLine()?.toDoubleOrNull() ?: return println("Entrada inválida!")
+// Variável imutável com a fórmula que descobre a grandeza da resistência.    
     val V = I * R
-    println("A tensão (V) é: $V Volts") // Variável imutável com a fórmula que descobre a grandeza da resistência.
+    println("A tensão (V) é: $V Volts") 
 }
 // Função que calcula a resistência de um material através da fórmula R= V/I.
 fun calcR() {
     print("Digite a tensão (V) em Volts: ")
-    val V = readLine()?.toDoubleOrNull() ?: return println("Entrada inválida!") // Faz com que se a variavel tensão for decimal ou nula irá retornar um println "Entrada inválida!", para evitar que a máquina dê erro.
+// Recebe a entrada do usuario e retorna erro caso seja inválida.
+    val V = readLine()?.toDoubleOrNull() ?: return println("Entrada inválida!") 
     print("Digite a corrente (I) em Amperes: ")
-    val I = readLine()?.toDoubleOrNull() ?: return println("Entrada inválida!") // Faz com que se a variavel intensidade elétrica for decimal ou nula irá retornar um println "Entrada inválida!", para evitar que a máquina dê erro.
+// Recebe a entrada do usuario e retorna erro caso seja inválida.
+    val I = readLine()?.toDoubleOrNull() ?: return println("Entrada inválida!") 
 // Condicional que verifica se a corrente elétrica for diferente de zero para a fórmula ser calculada normalmente.
     if (I != 0.0) {
         val R = V / I
         println("A resistência (R) é: $R Ohms")
     } 
-// Outra condicional que impôe uma segunda condicional, ou seja, se o número for 0 vai ser printado "Erro: Corrente não pode ser zero para calcular a resistência".    
+// Outra condicional que se o número da intensidade for 0 retorna erro.    
     else {
         println("Erro: Corrente não pode ser zero para calcular a resistência.")
     }
@@ -28,18 +33,22 @@ fun calcR() {
 // Função que calcula a intensidade elétrica através da fórmula I = V / R.
 fun calcI() {
     print("Digite a tensão (V) em Volts: ")
-    val V = readLine()?.toDoubleOrNull() ?: return println("Entrada inválida!") // Faz com que se a variavel tensão for decimal ou nula irá retornar um println "Entrada inválida!", para evitar que a máquina dê erro.
+// Recebe a entrada do usuario e retorna erro caso seja inválida.
+    val V = readLine()?.toDoubleOrNull() ?: return println("Entrada inválida!") 
     print("Digite a resistência (R) em Ohms: ")
-    val R = readLine()?.toDoubleOrNull() ?: return println("Entrada inválida!") // Faz com que se a variavel resistência for decimal ou nula irá retornar um println "Entrada inválida!", para evitar que a máquina dê erro.
+// Recebe a entrada do usuario e retorna erro caso seja inválida
+    val R = readLine()?.toDoubleOrNull() ?: return println("Entrada inválida!") 
 // Condicional que verifica se a resistência for diferente de zero para a fórmula ser calculada normalmente.    
     if (R != 0.0) { 
         val I = V / R
         println("A corrente (I) é: $I Amperes")
-    } 
+    }
+// Outra condicional que se o número da resistência for 0 retorna erro. 
     else {
         println("Erro: Resistência não pode ser zero para calcular a corrente.")
     }
-}  
+}
+// Método principal da aplicação  
 fun main() {
     println("Testando no JDoodle!") 
     var opcao: String
@@ -51,7 +60,8 @@ fun main() {
         println("3 - Calcular Corrente (I)")
         println("4 - Sair")
         print("Escolha uma opção:  " )
-        opcao = readLine() ?: "" // Permite que o programa lê a opção escolhida pelo usuário
+// Permite que o programa lê a opção escolhida pelo usuário
+        opcao = readLine() ?: "" 
 // Permite executar a função escolhida pelo menu do usuário
         when (opcao) {
             "1" -> calcV()
